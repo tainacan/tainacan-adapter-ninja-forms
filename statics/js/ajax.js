3781839de873304eeb1bea1080860d7d435199d7
vnmedeiros <vnicius.nm.ba@gmail.com>
2020-03-30 23:00:44 -0300

    add call of callback

--- statics/js/ajax.js
+++ statics/js/ajax.js
@@ -1,5 +1,5 @@
 
-function call_ajax(action, data) {
+function call_ajax(action, data, e, callback_sucess, callback_err) {
   //This does the ajax request
   jQuery.ajax({
     url: ajaxurl, // or example_ajax_obj.ajaxurl if using on frontend
@@ -8,10 +8,10 @@ function call_ajax(action, data) {
         'data' : data
     },
     success:function(data) {
-      console.log(data);
+      typeof callback_sucess === 'function' && callback_sucess(data, e);
     },
     error: function(errorThrown) {
-      console.log(errorThrown);
+      typeof callback_err === 'function' && callback_err(errorThrown, e);
     }
   });
 }
